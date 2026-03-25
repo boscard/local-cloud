48ed096876f24f026d1fd3432c3f2fbc9a10418c
Dawid Trzebiatowski <boscode@boscard.pl>
2026-03-25 20:31:03 +0100

    Finding backend

--- run.sh
+++ run.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env bash
 #set -x
 
+script_file="$(dirname $0)/$(basename $0)"
+
+if [[ -L "${script_file}" ]]
+then
+	script_file=$(readlink -f "${script_file}")
+fi
+
 : ${CONFIG_FILE:="config.yaml"}
 
 backend=$(yq -r .backend ${CONFIG_FILE})
 
-if [[ ${backend} != "null" ]] && [[ ! -f "backend-${backend}.sh" ]]
+if [[ ${backend} != "null" ]] && [[ ! -f "$(dirname ${script_file})/backend-${backend}.sh" ]]
 then
 	echo "Please configure valid backend to use!"
 	exit 254
 fi
 
-source ./backend-${backend}.sh
+source $(dirname ${script_file})/backend-${backend}.sh
 
 set -e
 set -o pipefail
